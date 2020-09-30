7b66de60526850103b6959ffa4b287244840bc83
anoopemacs <anoopemacs@gmail.com>
2020-09-30 06:04:43 +0530

    Starting mapping real data.json to props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
+//TODO: Keep it blind accessible
+//TODO: use propTypes to solidify json format
+
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
+
+import json_data from "./request_data.json";
+
+function Logo() {
+  return (
+    <div className="border0" alt="airbase logo">
+      airbase
+    </div>
+  );
+}
+
+function Security() {
+  return <div className="border0">Security0</div>;
+}
+
+function BoxHeading() {
+  return <div className="border0">BoxHeading0</div>;
+}
+
+function LeftBox() {
+  return <div className="border0">LeftBox</div>;
+}
+
+function RightBox({ approvers }) {
+  console.log(approvers);
+  return <div className="border0">RightBox</div>;
+}
+
+function ApproveDeny() {
+  return <div>Approve0, Deny0</div>;
+}
+
+function MainBox({ json_data }) {
+  console.log(json_data);
+  return (
+    <div className="border0">
+      <BoxHeading />
+      <LeftBox />
+      <RightBox approvers={json_data.approvers} />
+      <ApproveDeny />
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -19,6 +65,9 @@ function App() {
           Learn React
         </a>
 	  </header>*/}
+      <Logo />
+      <Security />
+      <MainBox json_data={json_data} />
     </div>
   );
 }
